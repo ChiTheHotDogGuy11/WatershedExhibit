a207239fa35ed8e47c55892959c0695df9b2c03e
Justin Greet <jgreet@andrew.cmu.edu>
2014-03-24 17:07:19 -0400

    Make scope of app.js global.

--- public/app.js
+++ public/app.js
@@ -1,46 +1,42 @@
-;
-jQuery(function($){    
-    'use strict';
+  'use strict';
 
-    /**
-     * All the code relevant to Socket.IO is collected in the IO namespace.
-     *
-     * @type {{init: Function, bindEvents: Function, onConnected: Function, onNewGameCreated: Function, playerJoinedRoom: Function, beginNewGame: Function, onNewWordData: Function, hostCheckAnswer: Function, gameOver: Function, error: Function}}
-     */
-    var IO = {
+/**
+ * All the code relevant to Socket.IO is collected in the IO namespace.
+ *
+ * @type {{init: Function, bindEvents: Function, onConnected: Function, onNewGameCreated: Function, playerJoinedRoom: Function, beginNewGame: Function, onNewWordData: Function, hostCheckAnswer: Function, gameOver: Function, error: Function}}
+ */
+var IO = {
 
-        /**
-         * This is called when the page is displayed. It connects the Socket.IO client
-         * to the Socket.IO server
-         */
-        init: function() {
-            IO.socket = io.connect();
-            IO.bindEvents();
-        },
+	/**
+	 * This is called when the page is displayed. It connects the Socket.IO client
+	 * to the Socket.IO server
+	 */
+	init: function() {
+		IO.socket = io.connect();
+		IO.bindEvents();
+	},
 
-        /**
-         * While connected, Socket.IO will listen to the following events emitted
-         * by the Socket.IO server, then run the appropriate function.
-         */
-        bindEvents : function() {
-            IO.socket.on('connected', IO.onConnected );
-            IO.socket.on('update', IO.onUpdate);
-        },
+	/**
+	 * While connected, Socket.IO will listen to the following events emitted
+	 * by the Socket.IO server, then run the appropriate function.
+	 */
+	bindEvents : function() {
+		IO.socket.on('connected', IO.onConnected );
+		IO.socket.on('update', IO.onUpdate);
+	},
 
-        /**
-         * The client is successfully connected!
-         */
-        onConnected : function(data) {
-            // Cache a copy of the client's socket.IO session ID on the App
-            //App.mySocketId = IO.socket.socket.sessionid;
-			alert("Connected by client!");
-        },
+	/**
+	 * The client is successfully connected!
+	 */
+	onConnected : function(data) {
+		// Cache a copy of the client's socket.IO session ID on the App
+		//App.mySocketId = IO.socket.socket.sessionid;
+		alert("Connected by client!");
+	},
 
-        onUpdate : function(data) {
-            console.log(data);
-        },
-    };
+	onUpdate : function(data) {
+		console.log(data);
+	},
+};
 
-    IO.init();
-
-}($));
+IO.init();
