3f76fe43583d20d4e6b7c852877a0ca86ee60410
Emily Tsai <cetsai@andrew.cmu.edu>
2014-03-24 23:46:13 -0400

    added geothermal gif

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,12 @@
 $(document).ready(function(){
 	$('.info_panel').hide();
 	$('#rainbarrel').hide();
+	$('#geothermal').hide();
 
 	$(".house").on("click", function(e){
 		$('.info_panel').fadeToggle('toggle');
 		$('#rainbarrel').toggle();
+		$('#geothermal').toggle();
 
 	});
 
