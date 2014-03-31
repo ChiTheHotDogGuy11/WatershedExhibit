c4eb04ae73534eaaa929f0d203bae6d722a4689a
yemount@gmail.com <shanhuan@andrew.cmu.edu>
2014-03-30 21:07:00 -0400

    data swirls mostly done.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
 // Insert JS here
 $(document).ready(function(){
+	loadSvg('images/info-circle.svg', 'info-circle', function(){});
+	loadSvg('images/info-icon.svg', 'info-icon', function(){})
+	loadSvg('images/house.svg', 'houseContainer', function(node){
+		var groupId = 0;
+		node.children('g').each(function(){
+			visibilities[groupId] = -1;
+			this.setAttribute('id', groupId+'g');
+			groupId++;
+		});
+		visibilities[0] = 0;
+		visibilities[1] = 0;
+		visibilities[2] = 0;
+		visibilities[3] = 0;
+		initTwo();
+	});
+
 	// $('.info_panel').hide();
 	$('#rainbarrel').hide();
 	$('#geothermal').hide();
